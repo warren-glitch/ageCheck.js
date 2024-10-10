53ddf236fcdca86e72e15d7c8a7a291706855584
warren-glitch <warrenpiano24@gmail.com>
2024-10-10 16:25:10 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let age = prompt("Enter your age:");
+
+if (age <13) {
+  alert("You are child.");
+}else if (age >= 13 && <= 19) {
+  alert("Your a teenager.");
+}else {
+  alert("You are an adult.")
+}
+
